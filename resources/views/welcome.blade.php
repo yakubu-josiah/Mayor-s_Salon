e8ec8b4a2a4ced6fb0c2ae61c00d7cb80f057afc
yakubu-josiah <jossysmothest007@gmail.com>
2023-03-14 20:09:14 +0100

    booking appointment form included

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -423,5 +423,113 @@
                 </div>
             </div>
         </section>
+            <!-- Appointment Form -->
+    <section class="testimonials">
+        <div class="background bg-img bg-fixed section-padding pb-0" data-background="img/slider/20.jpg" data-overlay-dark="6">
+            <div class="container">
+                <div class="row">
+                    <!-- Appointment call -->
+                    <div class="col-md-5 mb-30 mt-60">
+                        <p class="mb-0"><i class="star-rating"></i><i class="star-rating"></i><i class="star-rating"></i><i class="star-rating"></i><i class="star-rating"></i></p>
+                        <h5>We Are Best Barbers & Hair Cutting Salon at NYC.</h5>
+                        <div class="reservations mb-10">
+                            <div class="icon color-1"><span class="icon-icon-1-1"></span></div>
+                            <div class="text">
+                                <p class="color-1">Appointment</p> <a class="color-1" href="tel:855-100-4444">855 100 4444</a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Appointment form -->
+                    <div class="col-md-5 offset-md-2">
+                        <div class="booking-box">
+                            <div class="head-box text-center">
+                                <h4>Make An Appointment</h4>
+                            </div>
+                            <div class="booking-inner clearfix">
+                                <form class="form1 clearfix">
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <div class="input1_wrapper">
+                                                <label>Name</label>
+                                                <div class="input2_inner">
+                                                    <input type="text" class="form-control input" placeholder="Name" required>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="input1_wrapper">
+                                                <label>Phone</label>
+                                                <div class="input2_inner">
+                                                    <input type="text" class="form-control input" placeholder="Phone" required>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="input1_wrapper">
+                                                <label>Date</label>
+                                                <div class="input1_inner">
+                                                    <input type="text" class="form-control input datepicker" placeholder="Date" required>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="select1_wrapper">
+                                                <label>Time</label>
+                                                <div class="select1_inner">
+                                                    <select class="select2 select" style="width: 100%">
+                                                        <option value="0">Time</option>
+                                                        <option value="1">10:00 am</option>
+                                                        <option value="2">11:00 am</option>
+                                                        <option value="3">12:00 pm</option>
+                                                        <option value="4">14:00 pm</option>
+                                                        <option value="5">16:00 pm</option>
+                                                        <option value="6">18:00 pm</option>
+                                                        <option value="7">20:00 pm</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="select1_wrapper">
+                                                <label>Services</label>
+                                                <div class="select1_inner">
+                                                    <select class="select2 select" style="width: 100%">
+                                                        <option value="0">Services</option>
+                                                        <option value="0">Hair Styling</option>
+                                                        <option value="1">Face Mask</option>
+                                                        <option value="2">Shaving</option>
+                                                        <option value="3">Beard Triming</option>
+                                                        <option value="4">Hair Wash</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="select1_wrapper">
+                                                <label>Choose Barber</label>
+                                                <div class="select1_inner">
+                                                    <select class="select2 select" style="width: 100%">
+                                                        <option value="0">Choose Barber</option>
+                                                        <option value="0">Philip</option>
+                                                        <option value="1">Stephen</option>
+                                                        <option value="2">Dennis</option>
+                                                        <option value="3">Helen</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <button type="submit" class="btn-form1-submit mt-15">Make Appointment</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
     
 @endsection
