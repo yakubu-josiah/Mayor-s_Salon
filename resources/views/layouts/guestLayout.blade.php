686d86706c9cc902d5e0e6d70ca1e99a966228e4
yakubu-josiah <jossysmothest007@gmail.com>
2023-04-20 18:36:46 +0100

    adding the new designed favicon

--- resources/views/layouts/guestLayout.blade.php
+++ resources/views/layouts/guestLayout.blade.php
@@ -8,9 +8,9 @@
         <meta name="description" content="@yield('description') - {{ config('app.name') }}" />
         <link type="text/css" rel="stylesheet" id="dark-mode-custom-link">
         <link type="text/css" rel="stylesheet" id="dark-mode-general-link">
-        <link rel="shortcut icon" href="{{ asset('images/mayorFav.png') }}" />
-        <link rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700;800;900&display=swap">
+        <link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}" />
+        {{-- <link rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700;800;900&display=swap"> --}}
         <link rel="stylesheet" href="css/plugins.css" />
         <link rel="stylesheet" href="{{ asset('css/styles.css') }}" />
         <link rel="stylesheet" href="{{ asset('css/animate.css') }}" />
